b67565043ff112ea90b79233bf279a4abbda9b94
Jose Damian jldamians <jlds161089@gmail.com>
2019-04-09 13:44:13 -0500

    [update] Adecuando los test para validar los montos decimales

--- test/Sale.js
+++ test/Sale.js
@@ -34,7 +34,7 @@ describe('Prototype Sale.js', function() {
     newSale.exemptAmount = 100.00
     newSale.payableAmount = 100.00
 
-    assert.equal(newSale.getText(), '20190100|1|M-RER|01/01/2019||01|F001|1||6|20547872518|EASY TAXI PERÚ S.A.C.||||||100||||||100|PEN|1|||||||1|1|')
+    assert.equal(newSale.getText(), '20190100|1|M-RER|01/01/2019||01|F001|1||6|20547872518|EASY TAXI PERÚ S.A.C.||||||100.00||||||100.00|PEN|1.000|||||||1|1|')
   })
 
   it('Non taxable invoice (Inafecto)', function() {
@@ -51,7 +51,7 @@ describe('Prototype Sale.js', function() {
     newSale.nonTaxableAmount = 100.00
     newSale.payableAmount = 100.00
 
-    assert.equal(newSale.getText(), '20190100|2|M-RER|02/01/2019||01|F001|2||6|20547872518|EASY TAXI PERÚ S.A.C.|||||||100|||||100|PEN|1|||||||1|1|')
+    assert.equal(newSale.getText(), '20190100|2|M-RER|02/01/2019||01|F001|2||6|20547872518|EASY TAXI PERÚ S.A.C.|||||||100.00|||||100.00|PEN|1.000|||||||1|1|')
   })
 
   it('Taxable invoice (Gravado)', function() {
@@ -71,7 +71,7 @@ describe('Prototype Sale.js', function() {
     newSale.taxableAmount = 100.00
     newSale.payableAmount = 118.00
 
-    assert.equal(newSale.getText(), '20190100|3|M-RER|03/01/2019||01|F001|3||6|20547872518|EASY TAXI PERÚ S.A.C.||100||18||||||||118|PEN|1|||||||1|1|')
+    assert.equal(newSale.getText(), '20190100|3|M-RER|03/01/2019||01|F001|3||6|20547872518|EASY TAXI PERÚ S.A.C.||100.00||18.00||||||||118.00|PEN|1.000|||||||1|1|')
   })
 
   it('Taxable credit note (Gravado). Invoice issued the previous period', function() {
@@ -94,7 +94,7 @@ describe('Prototype Sale.js', function() {
     newSale.taxableAmount = 100.00
     newSale.payableAmount = 118.00
 
-    assert.equal(newSale.getText(), '20190100|1|M-RER|01/01/2019||07|F001|1||6|20547872518|EASY TAXI PERÚ S.A.C.|||-100||-18|||||||118|PEN|1|01/12/2018|01|F001|1|||1|1|')
+    assert.equal(newSale.getText(), '20190100|1|M-RER|01/01/2019||07|F001|1||6|20547872518|EASY TAXI PERÚ S.A.C.|||-100.00||-18.00|||||||118.00|PEN|1.000|01/12/2018|01|F001|1|||1|1|')
   })
 
   it('Taxable credit note (Gravado). Invoice issued the same period', function() {
@@ -117,6 +117,6 @@ describe('Prototype Sale.js', function() {
     newSale.taxableAmount = 100.00
     newSale.payableAmount = 118.00
 
-    assert.equal(newSale.getText(), '20190100|1|M-RER|10/01/2019||07|F001|2||6|20547872518|EASY TAXI PERÚ S.A.C.||100||18||||||||118|PEN|1|05/01/2019|01|F001|2|||1|1|')
+    assert.equal(newSale.getText(), '20190100|1|M-RER|10/01/2019||07|F001|2||6|20547872518|EASY TAXI PERÚ S.A.C.||100.00||18.00||||||||118.00|PEN|1.000|05/01/2019|01|F001|2|||1|1|')
   })
 })
